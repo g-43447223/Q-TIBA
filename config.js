22a93722aa9735001f11db80ef9b6c844b9e06b6
g-43447223 <g-43447223@moe-dl.edu.my>
2026-08-28 16:06:50 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Q-TIBA Configuration
+// Salin config.example.js kepada config.js dan masukkan URL anda.
+
+const API_URL = "https://script.google.com/macros/s/AKfycbz2T62_H3_zmXNvxsPxoqbaki8gZRXQ16cFSzIWOvKBfDuvakXOn50hQBXb2FxZF-XfPA/exec";
