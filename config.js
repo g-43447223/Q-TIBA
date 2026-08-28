872839899c92983aebb38a8d0ea903eb07b5c752
g-43447223 <g-43447223@moe-dl.edu.my>
2026-08-28 16:05:19 +0800

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Q-TIBA Configuration
-// Salin config.example.js kepada config.js dan masukkan URL anda.
-
-const API_URL = "https://script.google.com/macros/s/AKfycbxNvKcCq6dRAxfUDGiOzaSzT4TZWUO43HLUKRgD-l1mEl_vKSj5rT2MGM0m_bVTvdQquQ/exec";
