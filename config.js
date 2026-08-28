a47cb19e524daf8fb97feb5cf9c722402b71c4df
g-43447223 <g-43447223@moe-dl.edu.my>
2026-08-28 21:56:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Q-TIBA Configuration
 // Salin config.example.js kepada config.js dan masukkan URL anda.
 
-const API_URL = "https://script.google.com/macros/s/AKfycbz2T62_H3_zmXNvxsPxoqbaki8gZRXQ16cFSzIWOvKBfDuvakXOn50hQBXb2FxZF-XfPA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxPynJxFlcI8-cAPW1MeVG3_xNMcGI0Xr7M-Zeb9wiOI-uQ5YXChXC0fpjGrl-3Qg8p3g/exec";
