c3ccd43821fc2097c4ecd108b981e998fc3efe68
g-43447223 <g-43447223@moe-dl.edu.my>
2026-08-28 22:38:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 // Q-TIBA Configuration
 // Salin config.example.js kepada config.js dan masukkan URL anda.
 
-const API_URL = "https://script.google.com/macros/s/AKfycbxPynJxFlcI8-cAPW1MeVG3_xNMcGI0Xr7M-Zeb9wiOI-uQ5YXChXC0fpjGrl-3Qg8p3g/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzw2ffID1Am4VpO9VbaDkfXlO7LWoxHZQulJ1YFuBESP8ZykN1-Zhkwc8I_E5pZ9O2O6A/exec";
+
+// API Key untuk akses back end. WAJIB sama dengan var API_KEY dalam code.gs!
+const QTIBA_API_KEY = "QTiba-336244-CHANGE-ME-SEKRET";
